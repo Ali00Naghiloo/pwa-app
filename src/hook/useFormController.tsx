01b46f2251feed9bdc915cdf850f94ec6b033a23
Ali00Naghiloo <naghilooali0@gmail.com>
2023-08-31 20:03:10 +0330

    fix bug

--- src/hook/useFormController.tsx
+++ src/hook/useFormController.tsx
@@ -1,7 +1,10 @@
 import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 
 const useFormController = () => {
+  const navigate = useNavigate();
+
   const validationSchema = yup.object({
     lang: yup.string().required("Name is required"),
     gender: yup.string().required("Gender is required"),
@@ -15,10 +18,10 @@ const useFormController = () => {
     validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
-      console.log(values);
       localStorage.setItem("gender", values.gender);
       localStorage.setItem("lang", values.lang);
-      window.location.replace("http://localhost:5173/menu");
+      // window.location.replace("http://localhost:5173/menu");
+      navigate("/menu");
     },
   });
 
